dfe2f32e354c95ff3487e01c15cb4a72e857e033
tasman666 <a.trzepacz@gmail.com>
2015-03-12 19:07:11 +0100

    bowling almost working

--- src/main/scala/Bowling.scala
+++ src/main/scala/Bowling.scala
@@ -1,34 +1,29 @@
 class Bowling {
-  def score(frames: Frame*): Int =
-    frames.foldLeft((0: Int, NormalScore(0, 0): Score))((result: (Int, Score), frame: Frame) => {
-        val newScore: Score =
-          frame match {
-            case Frame('X') => Strike()
-            case Frame(x,y) if y =='/' => Spare(x)
-            case Frame(x,y) if y =='-' && x == '-' => NormalScore(0,0)
-            case Frame(x,y) if x =='-' => NormalScore(0, y.asDigit)
-            case Frame(x,y) if y =='-' => NormalScore(x.asDigit, x.asDigit)
-            case Frame(x,y) => NormalScore(x.asDigit, x.asDigit + y.asDigit)
-            case Frame(x,y,z) if y == '/' => Spare(x, 10)
-            case Frame(x,y,z) => Strike(x, x + y + z)
-          }
-        val scoreNumber = result._2 match {
-          case NormalScore(firstThrow, value) => newScore.value
-          case Spare(_,_) => newScore.value + newScore.firstThrow
-          case Strike(_,_) => newScore.value + newScore.value
-        }
-        (result._1 + scoreNumber, newScore)
+  def score(tries: String): Int = {
+    val throws: IndexedSeq[Throw] = tries.map(c => c match {
+      case 'X' => Strike()
+      case '/' => Spare()
+      case '-' => Zero()
+      case x => NormalThrow(x.asDigit)
+    });
+
+   throws.foldLeft((0, List[Throw]()))((result: (Int, List[Throw]), currentThrow: Throw) =>
+      result._2 match {
+        case Nil => (result._1 + currentThrow.value, List(currentThrow))
+        case h1 :: h2 :: tail if h2 == Strike() => (result._1 + currentThrow.value + (currentThrow.value + h1.value), currentThrow :: result._2)
+        case h1 :: h2 :: tail if h1 == Spare() => (result._1 + currentThrow.value + currentThrow.value - h2.value, currentThrow :: result._2)
+        case h :: tail => (result._1 + currentThrow.value, currentThrow :: result._2)
       }
-    )._1
-
-}
-case class Frame(tries: Char*)
-trait Score {
-  val firstThrow: Int;
-  val value: Int;
+   )._1
+  }
 }
 
-case class NormalScore(firstThrow: Int, value: Int) extends Score
-case class Strike(firstThrow:Int = 10, value:Int = 10) extends Score
-case class Spare(firstThrow: Int, value: Int = 10) extends Score
+trait Throw {
+  def value: Int;
+}
+
+case class Zero(value: Int = 0) extends Throw
+case class Strike(value:Int = 10) extends Throw
+case class Spare(value: Int = 10) extends Throw
+case class NormalThrow(value: Int) extends Throw
 
--- src/test/scala/BowlingTest.scala
+++ src/test/scala/BowlingTest.scala
@@ -5,8 +5,7 @@ class BowlingSuite extends FunSuite {
   test("bowling score 20 rolls: 10 pairs of 5 and 1") {
     val bowling: Bowling = new Bowling()
 
-    val result: Integer = bowling.score(Frame('5','1'),Frame('5','1'),Frame('5','1'),Frame('5','1'),Frame('5','1'),
-      Frame('5','1'), Frame('5','1'), Frame('5','1'), Frame('5','1'), Frame('5','1'))
+    val result: Integer = bowling.score("51515151515151515151")
 
     assert(result == 60)
   }
@@ -14,8 +13,7 @@ class BowlingSuite extends FunSuite {
   test("bowling score 20 rolls: 10 pairs of 9 and miss") {
     val bowling: Bowling = new Bowling()
 
-    val result: Integer = bowling.score(Frame('9','-'),Frame('9','-'),Frame('9','-'),Frame('9','-'),Frame('9','-'),
-      Frame('9','-'), Frame('9','-'), Frame('9','-'), Frame('9','-'), Frame('9','-'))
+    val result: Integer = bowling.score("9-9-9-9-9-9-9-9-9-9-")
 
     assert(result == 90)
   }
@@ -23,17 +21,15 @@ class BowlingSuite extends FunSuite {
   test("21 rolls: 10 pairs of 5 and spare, with a final 5") {
     val bowling: Bowling = new Bowling()
 
-    val result: Integer = bowling.score(Frame('5','/'),Frame('5','/'), Frame('5','/'), Frame('5','/'), Frame('5','/'),
-      Frame('5','/'), Frame('5','/'), Frame('5','/'), Frame('5','/'), Frame('5','/', '5'))
+    val result: Integer = bowling.score("5/5/5/5/5/5/5/5/5/5/5")
 
     assert(result == 150)
   }
 
-  test("bowling score 12 rolls: 12 strikess") {
+  test("bowling score 12 rolls: 12 strikes") {
     val bowling: Bowling = new Bowling()
 
-    val result: Integer = bowling.score(Frame('X'),Frame('X'),Frame('X'),Frame('X'),Frame('X'),Frame('X'),Frame('X'),
-      Frame('X'),Frame('X'),Frame('X', 'X', 'X'))
+    val result: Integer = bowling.score("XXXXXXXXXXXX")
 
     assert(result == 300)
   }
